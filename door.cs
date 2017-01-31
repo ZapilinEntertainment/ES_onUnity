6fd9debb13f49b56788ad0b9f33320f05087660c
ZapilinEntertainment <mahariusls@yandex.ru>
2017-02-01 02:08:24 +0500

    Add files via upload
    крепится при помощи hinge joint

---
+++ door.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class door : MonoBehaviour {
+	Quaternion start_rotation;
+	bool returning=false;
+	bool rest=true;
+	public float rotation_speed=30;
+	public float force_value=10;
+	Rigidbody rb;
+
+	void Start() {
+		start_rotation=transform.rotation;
+		rb=gameObject.GetComponent<Rigidbody>();
+	}
+
+	public void Activate(Vector3 pos) {
+		if (transform.rotation!=start_rotation) rest=false; else rest=true;
+		if (rest)	{
+			rb.AddForceAtPosition((transform.position-pos)*force_value, pos);
+			rest=false;
+		}
+		else {
+			rb.angularVelocity=Vector3.zero;
+			returning=true;
+
+		}
+	}
+
+	void Update () 
+	{
+		if (returning)
+		{
+			transform.rotation=Quaternion.RotateTowards(transform.rotation,start_rotation,rotation_speed*Time.deltaTime);
+			if (transform.rotation==start_rotation) {returning=false;rest=true;}
+		}
+	}
+
+
+}
